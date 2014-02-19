9da382a8cb4b4a35967381845d3f94952c7e044c
SongGithub <songjin@hotmail.com>
2014-02-20 08:19:07 +1000

    20140220 model setup

--- Proj_2DoList/to_do_list_app/models.py
+++ Proj_2DoList/to_do_list_app/models.py
@@ -1,20 +1,32 @@
 from django.db import models
+from django.utils import timezone
+import datetime
+
+
+
+class Category(models.Model):
+    Name_category=models.CharField(max_length=20)
+    Description_category=models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.Name_category
 
 class Item(models.Model):
+    category=models.ForeignKey(Category, related_name="Item_set")
+    
     Name_item=models.CharField(max_length=20)
-    Discription=models.CharField(max_length=100)
+    Description=models.CharField(max_length=100)
 
     Create_date=models.DateTimeField('date created')
     Due_date=models.DateTimeField('date due')
 
     Priority=models.IntegerField(default=0)
+    CompleteStatus=models.BooleanField(default=False)
+    
     def __unicode__(self):
         return self.Name_item
 
+    
 
 
-
-class Category(models.Model):
-    Name_item=models.CharField(max_length=20)
-    Discription=models.CharField(max_length=100)
 
