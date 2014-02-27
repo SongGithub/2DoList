4ae310ebc48510ebd00282f5eee9bbe86816f78f
SongGithub <songjin@hotmail.com>
2014-02-27 15:54:31 +1100

    assurance up2date code

--- Proj_2DoList/urls.py
+++ Proj_2DoList/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from to_do_list_app import views
 admin.autodiscover()
-#this give rights to edit in admin page
 
 urlpatterns = patterns('',
                        
